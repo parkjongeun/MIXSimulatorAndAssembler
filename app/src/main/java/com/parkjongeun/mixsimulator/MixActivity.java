78a0fbad7dea6b74cedb4b18f8159e55490d60ec
Park Jongeun <snowprint321@gmail.com>
2016-10-03 01:56:15 +0900

    Mix Activity.

--- app/src/main/java/com/parkjongeun/mixsimulator/MixActivity.java
+++ app/src/main/java/com/parkjongeun/mixsimulator/MixActivity.java
@@ -2,6 +2,12 @@ package com.parkjongeun.mixsimulator;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by Parkjongeun on 03/10/2016.
@@ -9,11 +15,53 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MixActivity extends AppCompatActivity {
 
+    RecyclerView mMemoryCells;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mix);
 
+        mMemoryCells = (RecyclerView) findViewById(R.id.memory_cells);
 
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        RecyclerView.Adapter adapter = new MemoryCellsAdapter();
+        mMemoryCells.setLayoutManager(layoutManager);
+        mMemoryCells.setAdapter(adapter);
+    }
+
+
+    private class MemoryCellsAdapter extends RecyclerView.Adapter<WordViewHolder> {
+
+
+        @Override
+        public WordViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = getLayoutInflater().inflate(R.layout.mix_word, parent, false);
+            return new WordViewHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(WordViewHolder holder, int position) {
+            holder.setAddress(position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return 4000;
+        }
+    }
+
+    private static class WordViewHolder extends RecyclerView.ViewHolder {
+
+        TextView mAddress;
+
+        public WordViewHolder(View itemView) {
+            super(itemView);
+            mAddress = (TextView) itemView.findViewById(R.id.address);
+        }
+
+        public void setAddress(int address) {
+            mAddress.setText(String.format("%04d: ", address));
+        }
     }
 }
